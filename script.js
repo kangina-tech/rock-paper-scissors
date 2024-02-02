981c845734744b6882c7eaee14fa28a70ed16d08
kangina-tech <debbienyasetia@gmail.com>
2024-02-02 14:05:22 +0300

    Added HTML and CSS properties.

--- script.js
+++ script.js
@@ -1,68 +1,62 @@
-const choice = ['rock', 'paper', 'scissors']; 
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice();
-let playerScore = 0;
-let computerScore = 0;
+const choices = ['rock', 'paper', 'scissors'];
 
-//computer play function
 function getComputerChoice() {
-    const randomNum = choice[Math.floor(Math.random() * choice.length)];
+    let randomNum = choices[Math.floor(Math.random() * choices.length)];
+    return randomNum;
 }
+const computerSelection = getComputerChoice();
 
-// player choice
 function getPlayerChoice() {
-    const playerChoice = prompt('Enter choice: ').toLowerCase;
-
-    if(!(choice.includes(playerSelection))) {
-        console.warn("Enter correct value");
-        return playerChoice();
+    const rockBtn = document.querySelector('.rock');
+    const paperBtn = document.querySelector('.paper');
+    const scissorsBtn = document.querySelector('.scissors');
+    
+    function onButtonClick() {
+        return playRound(playerSelection, computerSelection);
     }
+    
+    rockBtn.addEventListener('click', onButtonClick);
+    paperBtn.addEventListener('click', onButtonClick);
+    scissorsBtn.addEventListener('click', onButtonClick);
 }
+const playerSelection = getPlayerChoice();
 
-// play function
+const div = document.createElement('div');
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It's a tie";
-    } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
+        return "It's a tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper' || 
     playerSelection === 'paper' && computerSelection === 'scissors' || 
-    playerSelection === 'scissors' && computerSelection === "rock") {
+    playerSelection === 'scissors' && computerSelection === 'rock') {
         computerScore++;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
-    } else {
+    } else if (playerSelection === 'paper' && computerSelection === 'rock' || 
+    playerSelection === 'scissors' && computerSelection === 'paper' || 
+    playerSelection === 'rock' && computerSelection === 'paper') {
         playerScore++;
         return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return 'Please enter valid choice';
     }
 }
 
 
-playRound.addEventListener('click', () => {
-    const rock = document.querySelector('rock');
-    const paper = document.querySelector('paper');
-    const scissors = document.querySelector('scissors');
+function playGame() {
+    playerScore = 0;
+    computerScore = 0;
 
-    console.log(playRound(playerSelection, computerSelection));
-
-    const div = document.createElement('div');
-    
-    function game() {
-        gameScore = 0;
-
-        for(let i = 0; i < 5; i++) {
-            getComputerChoice();
-            getPlayerChoice();
-            console.log(playRound(playerSelection, computerSelection));
-        }
-        if (playerScore > computerScore) {
-            console.log('You win!');
-        }
-        else if (playerScore < computerScore) {
-            console.log('You lose!');
-        }
-        else {
-            console.log("It's a draw!");
-        }
+    for(let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
+        console.log(playRound(playerSelection, computerSelection));
     }
-    game();
-
-    div.appendChild(game);
-});
+    if(playerScore > computerScore) {
+        return 'You win!';
+    } else if(playerScore < computerScore) {
+        return 'You lose!';
+    } else {
+        return "It's a tie!";
+    }
+}
+playGame();
